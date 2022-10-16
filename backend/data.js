68dc0b765bf611f7f31e7440c616bd56be1d7bb3
StefaniaCodruta <stefaniamanaila2@gmail.com>
2022-10-16 15:37:50 +0100

    proiect ecommerce

---
+++ backend/data.js
@@ -0,0 +1,113 @@
+import bcrypt from 'bcryptjs';
+
+const data={
+    users: [
+        {
+          name: 'Stefania',
+          email: 'admin@example.com',
+          password: bcrypt.hashSync('123456'),
+          isAdmin: true,
+        },
+        {
+          name: 'John',
+          email: 'user@example.com',
+          password: bcrypt.hashSync('123456'),
+          isAdmin: false,
+        },
+      ],
+    products:[
+        {
+            // _id:'1',
+            name:'RANARP',
+            slug:'ranarap-lustra-alb',
+            category:'Corpuri de iluminat',
+            image:'/images/ranarp.jpg',
+            price:'149.90',
+            countInStock:10,
+            brand:'IKEA',
+            rating:4.5,
+            numReviews:10,
+            description:'Lămpile RANARP au un aspect retro, cu elemente și detalii precum îmbinările de oțel și cablul învelit cu material textil în dungi. Lampadarele și veiozele sunt grele și foarte stabile, dar complet reglabile.'
+        },
+        {
+            // _id:'2',
+            name:'MALM',
+            slug:'malm-comoda',
+            category:'Corpuri cu sertare',
+            image:'/images/malm.jpg',
+            price:'599',
+            countInStock:0,
+            brand:'IKEA',
+            rating:5,
+            numReviews:10,
+            description:'Comodă cu un design simplu care se potriveşte în dormitor sau în orice loc preferi să o aşezi. Sertarele rulează lin şi sunt disponibile în finisaje diferite - alege-ţi preferatul. Fixează mobilierul de perete.'
+        },
+        {
+            // _id:'3',
+            name:'HAUGA',
+            slug:'hauga-ansamblu-depozitare',
+            category:'Corpuri cu sertare',
+            image:'/images/hauga.jpg',
+            price:'1148',
+            countInStock:25,
+            brand:'IKEA',
+            rating:3.8,
+            numReviews:5,
+            description:'Folosește mobilierul în diverse locuri din casă, ca piesă independentă sau împreună cu alte piese din familia HAUGA. Polița superioară distinctă este o caracteristică comună a întregii familii HAUGA și îi conferă un stil clasic.'
+        },
+        {
+            // _id:'4',
+            name:'SOLLERÖN',
+            slug:'solleroen-fotoliu-exterior-maro',
+            category:'Mobilier de exterior',
+            image:'/images/solleroen.jpg',
+            price:'1149.60',
+            countInStock:18,
+            brand:'IKEA',
+            rating:4.0,
+            numReviews:8,
+            description:'Creează un spaţiu pentru relaxare cu secţiunile modulare SOLLERÖN. Din ratan plastic care nu necesită întreţinere şi cu perne rezistente şi cu protecţie antiderapantă.'
+        },
+        {
+            // _id:'5',
+            name:'BESTÅ',
+            slug:'besta-ansamblu-depozitare-cu-usi',
+            category:'Mobilier de exterior',
+            image:'/images/besta.jpg',
+            price:'1620',
+            countInStock:30,
+            brand:'IKEA',
+            rating:4.6,
+            numReviews:10,
+            description:'Lucrurile care tind să se aglomereze pot fi ascunse vederii, în timp ce obiectele preferate pot fi expuse pe polițe deschise. O soluție de depozitare și expunere este practică și poate fi personalizată.'
+        },
+        {
+            // _id:'6',
+            name:'BILD',
+            slug:'bild-afis-cochilii-pastelate',
+            category:'Decoratiuni perete',
+            image:'/images/bild.jpg',
+            price:'25.00',
+            countInStock:25,
+            brand:'IKEA',
+            rating:4.8,
+            numReviews:10,
+            description:'Poți da casei tale un aspect personalizat cu ajutorul obiectelor de artă care îți exprimă stilul.'
+        },
+        {
+            // _id:'7',
+            name:'GRADVIS',
+            slug:'gradvis-vaza-cu-accesoriu',
+            category:'Decoratiuni perete',
+            image:'/images/gradvis.jpg',
+            price:'45.00',
+            countInStock:50,
+            brand:'IKEA',
+            rating:5.0,
+            numReviews:18,
+            description:'Poți da casei tale un aspect personalizat cu ajutorul obiectelor de artă care îți exprimă stilul.'
+        }
+    ]
+};
+
+export default data;
---
+++ frontend/src/App.js
@@ -0,0 +1,134 @@
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
+import Navbar from 'react-bootstrap/Navbar';
+import Badge from 'react-bootstrap/Badge';
+import Nav from 'react-bootstrap/Nav';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import Container from 'react-bootstrap/Container';
+import { LinkContainer } from 'react-router-bootstrap';
+import { useContext } from 'react';
+import { Store } from './Store';
+import CartScreen from './screens/CartScreen';
+import SigninScreen from './screens/SigninScreen';
+import SignupScreen from './screens/SignupScreen';
+import DashboardScreen from './screens/DashboardScreen';
+import AdminRoute from './components/AdminRoute';
+import ProductListScreen from './screens/ProductListScreen';
+import ProductEditScreen from './screens/ProductEditScreen';
+
+function App() {
+  const { state, dispatch: ctxDispatch } = useContext(Store);
+  const { cart, userInfo } = state;
+
+  const signoutHandler = () => {
+    ctxDispatch({ type: 'USER_SIGNOUT' });
+    localStorage.removeItem('userInfo');
+  };
+  return (
+    <BrowserRouter>
+      <div className="d-flex flex-column site-container">
+        <ToastContainer position="bottom-center" limit={1} />
+        <header>
+          <Navbar bg="dark" variant="dark">
+            <Container>
+              <LinkContainer to="/">
+                <Navbar.Brand>Furniture-shop</Navbar.Brand>
+              </LinkContainer>
+              <Nav className="me-auto">
+                <Link to="/cart" className="nav-link">
+                  Cart
+                  {cart.cartItems.length > 0 && (
+                    <Badge pill bg="danger">
+                      {cart.cartItems.reduce((a, c) => a + c.quantity, 0)}
+                    </Badge>
+                  )}
+                </Link>
+                {userInfo ? (
+                  <NavDropdown title={userInfo.name} id="basic-nav-dropdown">
+                    <LinkContainer to="/profile">
+                      <NavDropdown.Item>User Profile</NavDropdown.Item>
+                    </LinkContainer>
+                    <LinkContainer to="/orderhistory">
+                      <NavDropdown.Item>Order History</NavDropdown.Item>
+                    </LinkContainer>
+                    <NavDropdown.Divider />
+                    <Link
+                      className="dropdown-item"
+                      to="#signout"
+                      onClick={signoutHandler}
+                    >
+                      Sign Out
+                    </Link>
+                  </NavDropdown>
+                ) : (
+                  <Link className="nav-link" to="/signin">
+                    Sign In
+                  </Link>
+                )}
+                 {userInfo && userInfo.isAdmin && (
+                    <NavDropdown title="Admin" id="admin-nav-dropdown">
+                      <LinkContainer to="/admin/dashboard">
+                        <NavDropdown.Item>Dashboard</NavDropdown.Item>
+                      </LinkContainer>
+                      <LinkContainer to="/admin/products">
+                        <NavDropdown.Item>Products</NavDropdown.Item>
+                      </LinkContainer>
+                      <LinkContainer to="/admin/orders">
+                        <NavDropdown.Item>Orders</NavDropdown.Item>
+                      </LinkContainer>
+                      <LinkContainer to="/admin/users">
+                        <NavDropdown.Item>Users</NavDropdown.Item>
+                      </LinkContainer>
+                    </NavDropdown>
+                  )}
+              </Nav>
+            </Container>
+          </Navbar>
+        </header>
+        <main>
+          <Container className="mt-3">
+            <Routes>
+              <Route path="/product/:slug" element={<ProductScreen />} />
+              <Route path="/cart" element={<CartScreen />} />
+              <Route path="/signin" element={<SigninScreen />} />
+              <Route path="/signup" element={<SignupScreen />} />
+              {/* Admin Routes */}
+              <Route
+                path="/admin/dashboard"
+                element={
+                  <AdminRoute>
+                    <DashboardScreen />
+                  </AdminRoute>
+                }
+              ></Route>
+              <Route
+                path="/admin/products"
+                element={
+                  <AdminRoute>
+                    <ProductListScreen />
+                  </AdminRoute>
+                }
+              ></Route>
+                 <Route
+                path="/admin/product/:id"
+                element={
+                  <AdminRoute>
+                    <ProductEditScreen />
+                  </AdminRoute>
+                }
+              ></Route>
+              <Route path="/" element={<HomeScreen />} />
+            </Routes>
+          </Container>
+        </main>
+        <footer>
+          <div className="text-center">All rights reserved</div>
+        </footer>
+      </div>
+    </BrowserRouter>
+  );
+}
+export default App;
